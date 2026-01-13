f5bc47e4dd9266ac3d584dd97e09422870f48c3b
Aanand Kumar <ak4328465@gmail.com>
2026-01-13 11:20:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,15 +2,15 @@ const searchInput = document.getElementById("search");
 const problems = document.querySelectorAll(".problem");
 
 searchInput.addEventListener("keyup", function () {
-  const searchText = searchInput.value.toLowerCase();
+  const value = searchInput.value.toLowerCase();
 
   problems.forEach(problem => {
     const text = problem.innerText.toLowerCase();
-
-    if (text.includes(searchText)) {
+    if (text.includes(value)) {
       problem.style.display = "block";
     } else {
       problem.style.display = "none";
     }
   });
 });
+
